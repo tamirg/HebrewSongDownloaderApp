1631c3a1c73a2e91afb499d5a903b56d5a983f4c
TamirG <tamirgold35@gmail.com>
2014-02-16 21:52:32 +0200

    Customized icons for the player and added forward and backward icons as well

--- src/com/alfa/HebrewSongDownloaderApp/PlayerFragment.java
+++ src/com/alfa/HebrewSongDownloaderApp/PlayerFragment.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
+import android.widget.ImageButton;
 
 /**
  * Created by Micha on 2/13/14.
@@ -15,8 +15,10 @@ import android.widget.Button;
 public class PlayerFragment extends Fragment {
     private MediaPlayer mPlayer;
     private MediaPlayer prevPlayer;
-    private Button playButton;
-    private Button stopButton;
+    private ImageButton playButton;
+    private ImageButton stopButton;
+    private ImageButton forwardButton;
+    private ImageButton backwardButton;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -34,6 +36,8 @@ public class PlayerFragment extends Fragment {
         this.mPlayer = mPlayer;
         playButton.setVisibility(View.VISIBLE);
         stopButton.setVisibility(View.VISIBLE);
+        forwardButton.setVisibility(View.VISIBLE);
+        backwardButton.setVisibility(View.VISIBLE);
         stopButton.setEnabled(false);
     }
 
@@ -49,29 +53,31 @@ public class PlayerFragment extends Fragment {
 
     public void startSong() {
         mPlayer.start();
-        playButton.setText("pause");
+        playButton.setImageResource(R.drawable.ic_pause);
         playButton.setEnabled(true);
         stopButton.setEnabled(true);
-
     }
 
     public void stopSong() {
         mPlayer.stop();
-        playButton.setText("play");
+        playButton.setImageResource(R.drawable.ic_play);
         playButton.setEnabled(false);
         stopButton.setEnabled(false);
     }
 
     public void pauseSong() {
         mPlayer.pause();
-        playButton.setText("play");
+        playButton.setImageResource(R.drawable.ic_play);
         stopButton.setEnabled(true);
     }
 
     private void setupFragmentView(View view) {
 
-        playButton = (Button) view.findViewById(R.id.playBtn);
-        stopButton = (Button) view.findViewById(R.id.stopBtn);
+        playButton = (ImageButton) view.findViewById(R.id.playBtn);
+        stopButton = (ImageButton) view.findViewById(R.id.stopBtn);
+        forwardButton = (ImageButton) view.findViewById(R.id.forwardBtn);
+        backwardButton = (ImageButton) view.findViewById(R.id.backwardBtn);
+
 
         // setup play button listener
         playButton.setOnClickListener(new View.OnClickListener() {
